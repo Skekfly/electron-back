e90fbcc7e351ec0a5538a974953ae184834bcdc6
fly <mail@flolep.com>
2021-05-14 00:39:58 +0200

    imports et démarrage simple

--- src/main/java/com/flolep/ElectronServer.java
+++ src/main/java/com/flolep/ElectronServer.java
@@ -1,8 +1,7 @@
 package com.flolep;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class ElectronServer {
@@ -10,20 +9,4 @@ public class ElectronServer {
 	public static void main(String[] args) {
 		SpringApplication.run(ElectronServer.class, args);
 	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
-	}
-
 }
